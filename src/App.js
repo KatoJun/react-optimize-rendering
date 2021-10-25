95ba723fdc573a458c94df687ef07f78bcbc3858
KatoJun <jun.kato0208@gmail.com>
2021-10-25 10:11:07 +0900

    useCallbackを使って最適化

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
-import { useState } from "react";
+// useCallback：処理が同じ場合は一度生成した関数を使い回すためのもの
+import { useState, useCallback } from "react";
 import { ChildArea } from "./ChildArea";
 import "./styles.css";
 
@@ -12,7 +13,9 @@ export default function App() {
   const onClickOpen = () => setOpen(!open);
 
   // アロー関数は毎回新しい関数を生成しているとみなされるのでChilcAreaのpropsが変わったと判断される
-  const onClickClose = () => setOpen(false);
+  // それを回避するためにuseCallbackを使って度生成した関数を使い回す。
+  // useCallbackはuseEffectと同様、引数に監視する変数を設定可能。空配列を設定すれば初回時のみ関数が生成される
+  const onClickClose = useCallback(() => setOpen(false), []);
 
   return (
     <div className="App">
